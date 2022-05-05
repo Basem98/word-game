d79bd9f0841e48424134c41dbf9e771fd1b1b263
Basem98 <basem.mostafa98@hotmail.com>
2022-05-05 19:14:03 +0200

    Update db connection options

--- backend/config/connectToDb.js
+++ backend/config/connectToDb.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = function connectToDatabase(url) {
-  mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }).then((hasConnected) => {
+  mongoose.connect(url).then((hasConnected) => {
     if (hasConnected) {
       console.log('Server has successfully connected to the database!');
     }
